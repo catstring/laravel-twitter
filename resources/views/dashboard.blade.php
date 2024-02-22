9968e1da9917eb4dd4f62c19dbbd0bbf0b5bb76d
Todd MC Tsai <toddmctsai@gmail.com>
2024-02-22 14:30:40 +0800

    fix search bar bugs

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -10,13 +10,15 @@
                 @include('shared.success-message')
                 @include('shared.submit-idea')
                 <hr>
-                @foreach ($ideas as $idea)
+                    @forelse ($ideas as $idea)
+                        <div class="mt-3">
+                            @include('shared.idea-card')
+                        </div>
+                    @empty
+                        <p class="text-center my-4">No results Found</p>
+                    @endforelse
                 <div class="mt-3">
-                    @include('shared.idea-card')
-                </div>
-                @endforeach
-                <div class="mt-3">
-                    {{ $ideas->links() }}
+                    {{ $ideas->withQueryString()->links() }}
                 </div>
             </div>
             <div class="col-3">
