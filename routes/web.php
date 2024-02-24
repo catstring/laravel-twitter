f5923c3196f7c5e8124a6b9d02f0dd6e64e089a1
Todd MC Tsai <toddmctsai@gmail.com>
2024-02-24 16:01:34 +0800

    #27 add profile edit bio, image

--- routes/web.php
+++ routes/web.php
@@ -51,6 +51,8 @@ Route::resource('ideas.comments',CommentController::class)->only(['store'])->mid
 
 Route::resource('users',UserController::class)->only('show','edit','update')->middleware('auth');
 
+Route::get('profile',[UserController::class,'profile'])->middleware('auth')->name('profile');
+
 // Route::get('/profile', [ProfileController::class , 'index']);
 
 Route::get('/terms', function () {
