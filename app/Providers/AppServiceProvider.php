55931db44f79b79d5cc5546eec53bfd20b3910eb
Todd MC Tsai <toddmctsai@gmail.com>
2024-05-09 20:41:27 +0800

    forceScheme

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use illuminate\Pagination\Paginator;
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -20,6 +21,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        Paginator::useBootstrapFive();
+        if(config('app.env') === 'production') {
+            URL::forceScheme('https');
+        }
+        // Paginator::useBootstrapFive();
     }
 }
