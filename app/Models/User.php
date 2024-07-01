d6ff8741e77885b9961c11b6e426e4507b2db58c
Todd MC Tsai <toddmctsai@gmail.com>
2024-07-01 12:20:46 +0800

    use avataaars

--- app/Models/User.php
+++ app/Models/User.php
@@ -80,6 +80,56 @@ class User extends Authenticatable
         if ($this->image) {
             return url('storage/' . $this->image);
         }
-        return "https://api.dicebear.com/6.x/fun-emoji/svg?seed={$this->name}";
-    }
+        // return "https://api.dicebear.com/6.x/fun-emoji/svg?seed={$this->name}";
+        // return "https://github.com/identicons/{$this->name}.png";
+        // return "https://robohash.org/{$this->name}.png";
+
+        // Define arrays for male and female features
+        $maleTopTypes = ["ShortHairShortCurly", "ShortHairShortFlat", "ShortHairShortRound"];
+        $femaleTopTypes = ["LongHairStraight", "LongHairCurly", "LongHairStraight2"];
+        $accessoriesTypes = ["Blank", "Kurt", "Prescription01", "Prescription02", "Round"];
+        $hairColors = ["Auburn", "Black", "Blonde", "BlondeGolden", "Brown"];
+        $facialHairTypes = ["Blank", "BeardMedium", "BeardLight", "MoustacheFancy"];
+        $facialHairColors = ["Auburn", "Black", "Blonde", "BlondeGolden", "Brown"];
+        $clotheTypes = ["BlazerShirt", "BlazerSweater", "CollarSweater", "GraphicShirt"];
+        $eyeTypes = ["Close", "Cry", "Default", "Dizzy", "EyeRoll"];
+        $eyebrowTypes = ["Angry", "Default", "FlatNatural", "RaisedExcited", "SadConcerned"];
+        $mouthTypes = ["Concerned", "Default", "Disbelief", "Eating", "Grimace"];
+        $skinColors = ["Tanned", "Yellow", "Pale", "Light", "Brown"];
+
+        $seed = $this->name;
+        $hash = md5($seed);
+
+        // Determine gender based on the hash
+        $isMale = hexdec(substr($hash, 0, 1)) % 2 === 0;
+        $topTypes = $isMale ? $maleTopTypes : $femaleTopTypes;
+
+        $avatarUrl = "https://avataaars.io/?avatarStyle=Circle"
+            . "&topType=" . $this->getRandomElement($hash, $topTypes, 1)
+            . "&accessoriesType=" . $this->getRandomElement($hash, $accessoriesTypes, 2)
+            . "&hairColor=" . $this->getRandomElement($hash, $hairColors, 3)
+            . "&clotheType=" . $this->getRandomElement($hash, $clotheTypes, 4)
+            . "&eyeType=" . $this->getRandomElement($hash, $eyeTypes, 5)
+            . "&eyebrowType=" . $this->getRandomElement($hash, $eyebrowTypes, 6)
+            . "&mouthType=" . $this->getRandomElement($hash, $mouthTypes, 7)
+            . "&skinColor=" . $this->getRandomElement($hash, $skinColors, 8)
+            . "&seed=" . urlencode($seed);
+
+        // Add facial hair parameters for male users only
+        if ($isMale) {
+            $avatarUrl .= "&facialHairType=" . $this->getRandomElement($hash, $facialHairTypes, 9)
+                . "&facialHairColor=" . $this->getRandomElement($hash, $facialHairColors, 10);
+        }
+
+        // Debugging output to verify gender-based logic
+        error_log("Generated avatar URL for user {$this->name} as " . ($isMale ? "male" : "female") . ": $avatarUrl");
+
+        return $avatarUrl;
+        }
+
+        private function getRandomElement($hash, $arr, $position)
+        {
+        $index = hexdec(substr($hash, $position, 1)) % count($arr);
+        return $arr[$index];
+        }
 }
